b76d096dc3b63659dc195b22db40712fdeeb49d4
Nick <nkowaleski@homex.com>
2021-06-30 15:06:13 -0400

    Remove debug code

--- src/scripts/config-ready.sh
+++ src/scripts/config-ready.sh
@@ -14,28 +14,19 @@ while true; do
 
   STATUS=$(jq -r '.data.attributes.status' <<< $RESPONSE_STATUS)
 
-  echo "$STATUS"
-
-  STATUS='pending'
-
-  echo "DEBUG: Entering if block"
   if [ $STATUS = 'uploaded' ]; then
     echo "Upload status confirmed!"
     exit 0
   elif [ $STATUS = 'pending' ]; then
-    if [ $attempts = 2 ]; then
+    if [ $ATTEMPTS = 2 ]; then
       echo "Status check timed out after 10s, exiting..."
       exit 1
     fi
     echo "Upload pending, retrying in 5s..."
-    echo "DEBUG: Calling increment of attempts"
     ATTEMPTS=$((ATTEMPTS + 1))
-    echo "DEBUG: Calling sleep"
     sleep 5
-    echo "DEBUG: Sleep over"
   elif [ $STATUS = 'errored' ] || [ $STATUS = 'null' ]; then
     echo "Configuration version $STATUS, exiting..."
     exit 1
   fi
-  echo "DEBUG: Reached end of elif block"
 done
